32a5c1321a9869f632843a1c6ba93e1288e6e1d4
Alex <alex.soussi@epitech.eu>
2026-06-08 12:05:57 +0200

    navbar almost done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,19 +3,19 @@ import './Navbar.css'
 function Navbar() {
 
   return (
-    <>
+    <div id='navbar'>
       <div id='title'>
         <h1>Alex SOUSSI</h1>
         <p>Concepteur Développeur d'Applications</p>
       </div>
       <div>
         <ul>
+          <li>Mon parcours</li>
           <li>Mes projets</li>
-          <li>LinkedIn</li>
-          <li>GitHub</li>
+          <li><a href='https://www.linkedin.com/in/alex-soussi-027aa62b3/' target='_blank'>LinkedIn</a></li>
         </ul>
       </div>
-    </>
+    </div>
   )
 }
 
