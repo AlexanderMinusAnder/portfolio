41113733c7d3c3246e491e8ea4bc2f1410b05e1e
Alexandre Moins andre <alexandremoinsandre@gmail.com>
2026-06-12 09:34:01 +0200

    navbar finished

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,39 +1,35 @@
 import './Navbar.css'
 import menu from "../../assets/menu.png"
 import menuSelected from "../../assets/menu-selected.png"
+import LinkedIn from "../../assets/LinkedIn.png"
 import { useState } from 'react'
 
 function Navbar() {
 
   const [selected, setSelected] = useState(false)
-  const [menuIcon, setMenuIcon] = useState(menu)
-  const [menuStyle, setMenuStyle] = useState('flex')
+  const [categorySelected, setCategorySelected] = useState(false)
 
-  const handleSelected = () => {
-    setSelected(previous => !previous)
-    if(selected && window.screen.width < 430) {
-      setMenuIcon(menuSelected)
-      setMenuStyle('none')
-    } else {
-      setMenuIcon(menu)
-      setMenuStyle('flex')
-    }
+  const handleSelected = (set: React.Dispatch<React.SetStateAction<boolean>>) => {
+    set(previous => !previous)
   }
 
   return (
-    <div id='navbar'>
-      <div id='title'>
+    <div className='nav'>
+      <div className='nav__title'>
         <h1>Alex SOUSSI</h1>
         <p>Concepteur Développeur d'Applications</p>
       </div>
-      <div id='menu'>
-        <ul style={{display: menuStyle}}>
+      <div className='nav__burger'>
+        <img src={selected ? menuSelected : menu} onClick={() => {handleSelected(setSelected)}} className='nav__burger-icon'></img>
+      </div>
+      <div className={`nav__menu${selected ? "--selected" : ""}`}>
+        <ul className='nav__category'>
           <li>Mon parcours</li>
+          <li>Mon expérience</li>
           <li>Mes projets</li>
-          <li><a href='https://www.linkedin.com/in/alex-soussi-027aa62b3/' target='_blank'>LinkedIn</a></li>
-          <li>Contacts</li>
+          {/* <li className='nav__category--linkedin'><a href='https://www.linkedin.com/in/alex-soussi-027aa62b3/' target='_blank'><img src={LinkedIn} />LinkedIn</a></li> */}
+          <li>Contacts et réseaux</li>
         </ul>
-        <img src={menuIcon} onClick={handleSelected} id='burger-menu'></img>
       </div>
     </div>
   )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,25 @@
-import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
+import code from "../../assets/code.png"
 
 function Home() {
 
   return (
-    <>
-    <h1>Présentation</h1>
-    </>
+    <div className='home'>
+      <div className='home__title'>
+        <h1>Présentation</h1>
+      </div>
+      <div className='home__content'>
+        <img src={code} />
+        <div className='home__text'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
